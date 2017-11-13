9820ee29dbdb1f89dd2ed2000e3a0296eae0e2aa
Mattia Romeo <mattiaromeo@gmail.com>
2017-11-13 12:05:08 +0100

    spring setup

---
+++ src/main/java/tdd_code_kata/mattia/Application.java
@@ -0,0 +1,13 @@
+package tdd_code_kata.mattia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
